130edbcde581a65ba4d0495d85c7ab4a93c81bd9
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-18 16:21:21 +0530

    Add files via upload

---
+++ Day-5-Varibles&Datatypes.py
@@ -0,0 +1,16 @@
+a= 7505.058263
+print(a)
+b= "Anvesh"
+print(b)
+c= True
+d= None
+print("The type of a is",type(a))
+print("The type of b is",type(b))
+print("The type of c is",type(c))
+
+list1 = [1,3,[4,6],["apple","banana"]]
+print(list1)
+
+# tupples are immutable
+# mutable chage kia jaa skta hai 
+# immutable change nhi kia jaa skta hai
---
+++ Day-6-Calculator.py
@@ -0,0 +1,18 @@
+# Exercise-1
+# alt+shift+down
+
+a=50
+b=3
+
+print("The value of" , a , "+",b, "is: ",a+b)
+
+print("The value of" , a , "-",b, "is: ",a-b)
+
+print("The value of" , a , "*",b, "is: ",a*b)
+
+print("The value of" , a , "/",b, "is: ",a/b)
+
+
+print("The value of" , a , "**",b, "is: ",a**b)
+
+ 
