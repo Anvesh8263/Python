172b38ddedb642cb70ce760e1e9664b69f68cc81
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-20 14:28:12 +0530

    Add files via upload

---
+++ Day-7-Typecasting.py
@@ -0,0 +1,16 @@
+# The Conversion of one data type into the other data type is known as type casting in python or type conversion in python
+
+# Explicit Conversion (Conversion doing myself)
+# Implicit Conversion (conversion doing by python interpreter)
+
+a=1
+b=2
+print(a+b)
+
+a="1"
+b="2"
+print(int(a)+int(b))
+
+c=1.5
+d=8
+print(c+d)
---
+++ Day-8-User_input.py
@@ -0,0 +1,6 @@
+a  = input("Enter your Name: ")
+print("My Name is",a)
+
+x = input("Enter your First Number: ")
+y = input("Enter your Second Number: ")
+z = print("The Total Number is: ", int(x)+int(y))
