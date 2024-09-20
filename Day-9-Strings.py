d66b143eea5974c52d11427cb170b84c6dd082e7
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-20 14:49:03 +0530

    Add files via upload

---
+++ Day-9-Strings.py
@@ -0,0 +1,16 @@
+name = "Anvesh"
+apple = '''Paragraphs are the building '''
+print("Hello "+ name)
+print(apple)
+print(name[0])
+print(name[1])
+print(name[2])  
+print(name[3])
+print(name[4]) 
+print(name[5])
+# print(name[6]) This throws an error because indexing range out
+# indexing begins with zero in string
+# indexing begins with zero in Array also
+
+for character in apple:
+    print(character)
