bbcab496e4e1e5f5e58df035a27f916891e1dd12
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 19:31:31 +0530

    Add files via upload

---
+++ Day-15-Loops.py
@@ -0,0 +1,28 @@
+# Loops
+# for Loop
+# while Loop
+
+
+Name = "Anvesh"
+for i in Name:
+    print(i)
+    if(i=="n"):
+        print("This is Special Character")
+
+
+# Another Example
+
+color = ["Red","Blue","Green","Yellow","Purple"]
+for i in color:
+    print(i)
+    for c in i:
+        print(c)
+
+
+# Another Example
+
+for k in range(20):
+    print(k)
+
+for k in range(1,10):
+    print(k+1)
