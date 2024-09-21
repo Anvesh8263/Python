f9834df040cc345a01d271ca8534ffdb4b3605f2
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 12:38:57 +0530

    Create Day-12-If-Else(Example-3).py

---
+++ Day-12-If-Else(Example-3).py
@@ -0,0 +1,8 @@
+appleprice = 10
+budget = 100
+if(budget - appleprice >50):
+    print("Take 1kg of Apple")
+elif(budget-appleprice > 70):
+    print("Its ok you can Buy")
+else:
+    print("You cannot buy an Apples")
