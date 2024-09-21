459eec0659dfdd2be8e5c6fa77f48225123405ca
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 12:37:25 +0530

    Create Day-12if-Else(Example-1).py

---
+++ Day-12if-Else(Example-1).py
@@ -0,0 +1,9 @@
+a1 = input("Enter Your Name: ")
+print("Hello",a1,"Hows your Age")
+a = int(input("Enter your age: "))
+print("Your Age is: ",a)
+
+if(a>18):
+    print("You can Drive a Car")
+else:
+    print("You cannot drive a Car")
