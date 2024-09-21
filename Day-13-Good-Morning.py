868b3e54bed4460e6bfed8f8208dfe2ec0ba111f
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 13:03:47 +0530

    Add files via upload

---
+++ Day-13-Good-Morning.py
@@ -0,0 +1,13 @@
+# The Strftime() function is used to convert date and time objects to their string representation.
+
+import time
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+timestamp = time.strftime('%H')
+print(timestamp)
+timestamp = time.strftime('%M')
+print(timestamp)
+timestamp = time.strftime('%S')
+print(timestamp)
+
+
