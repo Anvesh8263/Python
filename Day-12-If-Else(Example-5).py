e9136ac2cba22e124fd3150fad9dff96c53aedb7
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 12:40:15 +0530

    Create Day-12-If-Else(Example-5).py

---
+++ Day-12-If-Else(Example-5).py
@@ -0,0 +1,9 @@
+num = int(input("Enter the Value of num: "))
+if(num<0):
+    print("Number is Positive")
+elif(num==0):
+    print("Number is Zero")
+elif(num==999):
+    print("Number is Special")
+else:
+    print("Number is Negative")
