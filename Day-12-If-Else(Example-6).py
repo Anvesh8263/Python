fb3fb00010ff695cc561fc0216f8232c9de6b031
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-09-21 12:40:40 +0530

    Create Day-12-If-Else(Example-6).py

---
+++ Day-12-If-Else(Example-6).py
@@ -0,0 +1,12 @@
+num =18
+if(num<0):
+    print("Nummber is Negative")
+elif(num>0):
+    if(num <=10):
+        print("Number is Between 10-20")
+    elif(num>10 and num<=20):
+        print("Number is Between 10-20")
+    else:
+        print("Number is greater than 20")
+else:
+    print("Number is Zero")
