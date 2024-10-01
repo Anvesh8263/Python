63e7917ed923e721cfe841cae3b9069bab9779f9
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-10-01 12:13:24 +0530

    Create Day-51-Questions.py

---
+++ Day-51-Questions.py
@@ -0,0 +1,9 @@
+# Print characters present at an even index number
+
+a = input("Enter the Name: ")
+print("Orginal String is: ", a)
+
+print("Printing only even index chars")
+
+for i in a:
+    print(i)
