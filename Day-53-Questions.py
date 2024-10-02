b2a479531461cf49bed561c29e0a29aa78f4acdc
Anvesh Mangalam <80801707+Anvesh8263@users.noreply.github.com>
2024-10-02 15:03:58 +0530

    Create Day-53-Questions.py

---
+++ Day-53-Questions.py
@@ -0,0 +1,8 @@
+# Display numbers divisible by 5
+
+num_List = [10,20,33,46,55]
+print("Given list is: ", num_List)
+print("Divisible by 5:")
+for num in num_List:
+    if num % 5 ==0:
+        print(num)
